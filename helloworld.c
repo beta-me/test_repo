f7c095592ca8e1ccedcf66741754b8c3d3e7a5d6
beta_me <beta.me.beta@gmail.com>
2020-04-16 12:13:51 +0530

    added lines to print the difference

--- helloworld.c
+++ helloworld.c
@@ -7,6 +7,7 @@ int main()//main function
     int a=0,b=5;
     printf("sum = a+b=%d\n",a+b);//prints the sum
     printf("product = 0 as a is 0 so a*b=0\n");
+    printf("difference is a-b=%d\n",a-b);//prints the difference
     return a;//returns main function
 }
 
